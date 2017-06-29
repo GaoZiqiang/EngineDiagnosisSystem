b28624d1a03caf53a385e5e1c5ee1438e38cd8ae
gaoziqiang <gaoziqianghi@163.com>
2017-06-29 16:50:38 +0800

    UserControllerController添加验证器

--- src/main/java/cn/edu/sdut/softlab/controller/UserController.java
+++ src/main/java/cn/edu/sdut/softlab/controller/UserController.java
@@ -1,8 +1,14 @@
 package cn.edu.sdut.softlab.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,6 +19,7 @@ import javax.transaction.HeuristicRollbackException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 
+import cn.edu.sdut.softlab.model.EngineInfo;
 import cn.edu.sdut.softlab.model.TempUserInfo;
 import cn.edu.sdut.softlab.model.UserInfo;
 
@@ -22,7 +29,9 @@ public class UserController {
 	EntityManagerFactory emf;
 	EntityManager em;
 
-	private UserInfo user = new UserInfo();
+	private String tempLoginName;
+	@Inject
+	private UserInfo user;
 	private TempUserInfo tempUser = new TempUserInfo();
 
 	public UserInfo getUser() {
@@ -42,73 +51,174 @@ public class UserController {
 
 	}
 
-	// 用户注册
+	/*
+	 * * 用户注册
+	 */
 	public String register() throws IllegalStateException, SecurityException, HeuristicMixedException,
 			HeuristicRollbackException, RollbackException, SystemException {
 		try {
-			// utx.begin();
 			// 该参数必须与persistence-unit的name相一致
-			System.out.println("Start!");
 			emf = Persistence.createEntityManagerFactory("engine_diagnosis_system");
-
+			//断点测试
+			System.out.println("断点测试emf:  " + emf.toString());
 			em = emf.createEntityManager();
-			System.out.println("打印输出em:  " + em.toString());
-			System.out.println("打印输出emf:  " + emf.toString());
-
-			// 断点测试
-			System.out.println("打印输出newPerson:   " + user.toString());
-			System.out.println("打印输出em:  " + em.toString());// 测试结果，EntityManager注入失败
-			em.getTransaction().begin();// 至关重要的一步：开启事务
-			// 主键自动生成策略 就不能再setId了
-			// user.setId(365);
-
+			// 开启事务
+			em.getTransaction().begin();
+			// 将User持久化到数据库
 			em.persist(user);
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			em.close();
-
 			emf.close();
-			// utx.commit();
-			System.out.println("存入成功!");
+			System.out.println("注册成功!");
 		}
-		return "diagnosis.jsf";
+		return "diagnosis";
 	}
 
-	// 用户登录
-	public String login() {
+	/*
+	 * * 用户登录
+	 */
+	public String login() throws IllegalStateException, SecurityException, HeuristicMixedException,
+			HeuristicRollbackException, RollbackException, SystemException {
 		String flag = null;
 		try {
 			emf = Persistence.createEntityManagerFactory("engine_diagnosis_system");
 			em = emf.createEntityManager();
 
-			System.out.println("打印日志em in login:" + em.toString());
 			Query query = em
 					.createQuery("select u from UserInfo u where u.username = :username and u.password = :password");
 			query.setParameter("username", tempUser.getUsername());
 			query.setParameter("password", tempUser.getPassword());
 
 			List resultList = query.getResultList();
-			System.out.println("打印日志resultListSize:" + resultList.size());
-			if (resultList.size() > 0) {
-				flag = "error";
+
+			if (resultList.size() == 1) {
+				flag = "diagnosis";
 			} else {
-				flag = "diagnosis.jsf";
+				flag = "index";
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return flag;
+		return "diagnosis.jsf";
 
 	}
+
+	public boolean loginValidate1(String username) throws IllegalStateException, SecurityException,
+			HeuristicMixedException, HeuristicRollbackException, RollbackException, SystemException {
+		boolean flagBoolean = false;
+		try {
+			emf = Persistence.createEntityManagerFactory("engine_diagnosis_system");
+			em = emf.createEntityManager();
+
+			Query query = em.createQuery("select u from UserInfo u where u.username = :username");
+			query.setParameter("username", username);
+			// query.setParameter("password", tempUser.getPassword());
+
+			List resultList = query.getResultList();
+
+			/*
+			 * Iterator<UserInfo> userIterator = resultList.iterator(); while
+			 * (userIterator.hasNext()) { UserInfo newUser = (UserInfo)
+			 * userIterator.next(); // item.getName(); tempLoginName =
+			 * newUser.getUsername(); System.out.println("print newId:  " +
+			 * newUser.getId()); }
+			 */
+
+			if (resultList.size() == 1) {
+				flagBoolean = true;
+			} else {
+				flagBoolean = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flagBoolean;
+
+	}
+
+	public boolean loginValidate2(String password) throws IllegalStateException, SecurityException,
+			HeuristicMixedException, HeuristicRollbackException, RollbackException, SystemException {
+		boolean flagBoolean = false;
+		try {
+			emf = Persistence.createEntityManagerFactory("engine_diagnosis_system");
+			em = emf.createEntityManager();
+
+			Query query = em
+					.createQuery("select u from UserInfo u where u.username = :username and u.password = :password");
+			query.setParameter("username", tempUser.getUsername());
+			query.setParameter("password", password);
+
+			List resultList = query.getResultList();
+
+			/*
+			 * Iterator<UserInfo> userIterator = resultList.iterator(); while
+			 * (userIterator.hasNext()) { UserInfo newUser = (UserInfo)
+			 * userIterator.next(); // item.getName(); tempLoginName =
+			 * newUser.getUsername(); System.out.println("print newId:  " +
+			 * newUser.getId()); }
+			 */
+
+			if (resultList.size() == 1) {
+				flagBoolean = true;
+			} else {
+				flagBoolean = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flagBoolean;
+
+	}
+
 	/*
-	 * Query query = em.
-	 * createQuery("select u from UserInfo u where u.username = :uername and u.password = :password"
-	 * ); query.setParameter("username", tempUser.getUsername());
-	 * query.setParameter("password", tempUser.getPassword());
-	 * 
-	 * List resultList = query.getResultList(); if(resultList.size() > 0) {
-	 * return "index"; }else { return "error"; } }
+	 * * 用户名验证 长度 用户注册
 	 */
+	public void userNameValidate(FacesContext context, UIComponent component, Object value)
+			throws IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException,
+			RollbackException, SystemException {
+		if (value.toString().length() < 5 || value.toString().length() > 20) {
+			throw new ValidatorException(new FacesMessage("用户名长度应为5-20个字符"));
+		}
+		if (loginValidate1(value.toString())) {
+			throw new ValidatorException(new FacesMessage("该用户名已注册"));
+		}
+	}
+
+	/*
+	 * s * 用户密码验证 长度 用户注册
+	 */
+	public void passwordValidate(FacesContext context, UIComponent component, Object value)
+			throws IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException,
+			RollbackException, SystemException {
+		if (value.toString().length() < 5 || value.toString().length() > 12) {
+			throw new ValidatorException(new FacesMessage("密码长度应为5-12个字符"));
+		}
+
+	}
+
+	/*
+	 * * 用户名验证 用户登录
+	 */
+	public void loginUserNameValidate(FacesContext context, UIComponent component, Object value)
+			throws IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException,
+			RollbackException, SystemException {
+		if (!loginValidate1(value.toString())) {
+			throw new ValidatorException(new FacesMessage("该用户不存在"));
+		}
+	}
+
+	/*
+	 * * 用户名密码验证 用户登录
+	 */
+	public void loginPasswordValidate(FacesContext context, UIComponent component, Object value)
+			throws IllegalStateException, SecurityException, HeuristicMixedException, HeuristicRollbackException,
+			RollbackException, SystemException {
+		if (!loginValidate2(value.toString())) {
+			throw new ValidatorException(new FacesMessage("密码错误"));
+		}
+	}
+
 }
